e9d4dfe741a57a1ab1b146feb839fabe79e3d5f2
Gennadiy Chernyshyk <genaloner@gmail.com>
2018-08-23 20:42:07 +0300

    Implement wolf random movement

--- Scripts/Creatures/Wolf.gd
+++ Scripts/Creatures/Wolf.gd
@@ -1,42 +1,86 @@
 extends "res://Scripts/Creatures/Creature.gd"
 
+var currentAction
+enum {NONE, WALKING, HOWLING}
+
 func _ready():
-	_randomize_velocity()
+	_on_ActionTimer_timeout()
 	
 func _process(delta):
-	if (move_and_collide(velocity * delta) != null || velocity.length() == 0):
-		# If creature collides
-		_randomize_velocity()
-	else:
-		# Animate movement
-		if velocity.x == 0 and velocity.y < 0:
-			if $Body/Animation.current_animation != "WalkUp":
-				$Body/Animation.play("WalkUp")
-		elif velocity.x < 0:
-			if $Body/Animation.current_animation != "WalkLeft":
-				$Body/Animation.play("WalkLeft")
-		elif velocity.x == 0 and velocity.y > 0:
-			if $Body/Animation.current_animation != "WalkDown":
-				$Body/Animation.play("WalkDown")
-		elif velocity.x > 0:
-			if $Body/Animation.current_animation != "WalkRight":
-				$Body/Animation.play("WalkRight")
+	if currentAction == WALKING:
+		if (move_and_collide(velocity * delta) != null):
+			# If creature collides
+			_randomize_velocity()
 		else:
-			match $Body/Animation.current_animation:
-				"up":
-					$Body.set_frame(5)
-				"left":
-					$Body.set_frame(15)
-				"down":
-					$Body.set_frame(0)
-				"right":
-					$Body.set_frame(10)
-				_:
-					$Body/Animation.stop()
+			# If moving successful
+			_walk_animation()
+
+# Wait some time before movement
+func _on_ActionTimer_timeout():
+	var action
+	var time = OS.get_time().hour
+	if time > 21 || time < 4:
+		action = randi() % 3
+	else:
+		action = randi() % 2
+	
+	match action:
+		NONE:
+			_stop_animation()
+			$ActionTimer.wait_time = 1
+		WALKING:
+			_randomize_velocity()
+			$ActionTimer.wait_time = randi() % 5 + 2
+		HOWLING:
+			_howl_animation()
+			$ActionTimer.wait_time = 1
+	currentAction = action
+	$ActionTimer.start()
 
 func _randomize_velocity():
-	$RandomTimer.wait_time = randi() % 9 + 1
-	$RandomTimer.start()
 	velocity = Vector2(randi() % 3 - 1, randi() % 3 - 1)
 	velocity = velocity.normalized() * speed / 2
-	
+	if velocity.length() == 0:
+		# If generated velocity is equal to zero
+		_randomize_velocity()
+
+func _stop_animation():
+	$Body/Animation.stop()
+	match direction:
+		UP:
+			$Body.set_frame(5)
+		LEFT:
+			$Body.set_frame(15)
+		DOWN:
+			$Body.set_frame(0)
+		RIGHT:
+			$Body.set_frame(10)
+
+func _walk_animation():
+	if velocity.x == 0 and velocity.y < 0:
+		if $Body/Animation.current_animation != "WalkUp":
+			direction = UP
+			$Body/Animation.play("WalkUp")
+	elif velocity.x < 0:
+		if $Body/Animation.current_animation != "WalkLeft":
+			direction = LEFT
+			$Body/Animation.play("WalkLeft")
+	elif velocity.x == 0 and velocity.y > 0:
+		if $Body/Animation.current_animation != "WalkDown":
+			direction = DOWN
+			$Body/Animation.play("WalkDown")
+	elif velocity.x > 0:
+		if $Body/Animation.current_animation != "WalkRight":
+			direction = RIGHT
+			$Body/Animation.play("WalkRight")
+
+func _howl_animation():
+	match direction:
+		UP:
+			$Body/Animation.play("HowlUp")
+		LEFT:
+			$Body/Animation.play("HowlLeft")
+		DOWN:
+			$Body/Animation.play("HowlDown")
+		RIGHT:
+			$Body/Animation.play("HowlRight")
