e672b7052f6d6c630c7aceb8c1e93ece9d381a27
Gennadiy Chernyshyk <genaloner@gmail.com>
2018-11-18 23:58:37 +0200

    Start damage timer only for server

--- Scripts/Effects/Fire.gd
+++ Scripts/Effects/Fire.gd
@@ -6,16 +6,17 @@ func _ready():
 	$Sprite/Animation.play("fire")
 
 func _on_Fire_body_entered(body):
-	$DamageTimer.start()
+	# Damage to bodies when timer ends
+	if get_tree().is_network_server():
+		$DamageTimer.start()
 	
 func _on_Fire_body_exited(body):
 	if get_overlapping_bodies().empty():
 		$DamageTimer.stop()
 
 func _on_DamageTimer_timeout():
-	if get_tree().is_network_server():
-		for body in get_overlapping_bodies():
-			if body.is_in_group("Players") and body.health > 0:
-				body.health -= randi() % 3 + 1
-				body.rset("health", body.health)
-				print(body.health)
+	for body in get_overlapping_bodies():
+		if body.is_in_group("Players") and body.health > 0:
+			body.health -= randi() % 3 + 1
+			body.rset("health", body.health)
+			print(body.health)
