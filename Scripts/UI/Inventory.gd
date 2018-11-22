137017c16e2a9499d830d394c2101f8a6c96b7f7
Gennadiy Chernyshyk <genaloner@gmail.com>
2018-11-22 16:41:54 +0200

    Stylize inventory

--- Scripts/UI/Inventory.gd
+++ Scripts/UI/Inventory.gd
@@ -1,7 +1,10 @@
 extends WindowDialog
 
+const empty_slot = preload("res://Sprites/Items/EmptySlot.png")
+
 func _ready():
-	pass
+	for i in range(18):
+		$ItemList.add_icon_item(empty_slot, false)
 	
 func _process(delta):
 	if Input.is_action_just_pressed("ui_inventory"):
