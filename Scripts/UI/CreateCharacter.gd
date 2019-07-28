5f37b5c2726aba5ec31f0a03c0730c0f0d2ed7c9
Shatur95 <genaloner@gmail.com>
2019-07-28 15:22:43 +0300

    Randomize character at start

--- Scripts/UI/CreateCharacter.gd
+++ Scripts/UI/CreateCharacter.gd
@@ -1,19 +1,35 @@
 extends Control
 
 func _ready() -> void:
+	randomize_character()
 	$Character/Animation.play_directional_animation("Walk")
-	$Character/HealthBar.visible = false
-	$Character/Name.visible = false
-	
-	$Panel/GridContainer/HairColor.color = Color(randf(), randf(), randf())
-	$Panel/GridContainer/Skintone.color = Color(randf(), randf(), randf())
-	$Panel/GridContainer/Eyes.color = Color(randf(), randf(), randf())
 
-func _change_nickname(nickname: String) -> void:
+func _check_nickname(nickname: String) -> void:
 	$Character.game_name = nickname
 	$Done.disabled = nickname.empty()
 
-func _change_sex(id: int) -> void:
+func randomize_character() -> void:
+	var sex : int = randi() % $Panel/GridContainer/Sex.get_item_count()
+	var race : int = randi() % $Panel/GridContainer/Race.get_item_count()
+	var hair : int = randi() % $Panel/GridContainer/Hair.get_item_count()
+	var hair_color : Color = Color(randf(), randf(), randf())
+	var eyes_color : Color = Color(randf(), randf(), randf())
+	
+	# Set data in controls
+	$Panel/GridContainer/Sex.select(sex)
+	$Panel/GridContainer/Race.select(race)
+	$Panel/GridContainer/Hair.select(hair)
+	$Panel/GridContainer/HairColor.color = hair_color
+	$Panel/GridContainer/Eyes.color = eyes_color
+	
+	# Update player
+	set_sex(sex)
+	set_race(race)
+	set_hair(hair)
+	set_hair_color(hair_color)
+	set_eyes_color(eyes_color)
+
+func set_sex(id: int) -> void:
 	# Disable some hairs for male (id = 0)
 	for i in range(17, 30):
 		$Panel/GridContainer/Hair.set_item_disabled(i, !id)
@@ -25,23 +41,21 @@ func _change_sex(id: int) -> void:
 	
 	$Character.sex = $Panel/GridContainer/Sex.get_item_text(id)
 
-func _change_race(id: int) -> void:
+func set_race(id: int) -> void:
 	$Character.race = $Panel/GridContainer/Race.get_item_text(id)
 
-func _change_hair(id: int) -> void:
+func set_hair(id: int) -> void:
 	$Character.hair = $Panel/GridContainer/Hair.get_item_text(id)
 
-func _change_hair_color(color: Color) -> void:
+func set_hair_color(color: Color) -> void:
 	$Character.hair_color = color
 
-func _change_eyes_color(color: Color) -> void:
+func set_eyes_color(color: Color) -> void:
 	$Character.eyes = color
 
 func _change_skintone_color(color: Color) -> void:
 	$Character.skintone = color
 
-func _on_Done_pressed() -> void:
-	# Save properties
-	Global.player = $Character.data()
-	
-	var _error = get_tree().change_scene("res://Scenes/UI/JoinHost.tscn")
+func done() -> void:
+	Global.player = $Character.data() # Save properties
+	get_tree().change_scene("res://Scenes/UI/JoinHost.tscn")
